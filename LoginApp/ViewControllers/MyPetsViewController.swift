efc40967f3b04adcdeb5c7de9d86b0e347008372
Elisei Mishchenko <esm.mail@icloud.com>
2021-12-22 19:20:56 +0300

    New files for the view controller.

---
+++ LoginApp/ViewControllers/MyPetsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MyPetsViewController.swift
+//  LoginApp
+//
+//  Created by Елисей Мищенко on 22.12.2021.
+//
+
+import UIKit
+
+class MyPetsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
---
+++ LoginApp/ViewControllers/MyProfileViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyProfileViewController.swift
+//  LoginApp
+//
+//  Created by Елисей Мищенко on 22.12.2021.
+//
+
+import UIKit
+
+class MyProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
